330d915775b043f104be3148812a21a8ae4227ca
Rafael <dvr.rafa@gmail.com>
2022-11-22 10:45:56 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
@@ -13,8 +15,6 @@ function App() {
           </div>
         </section>
       </main>
-
-      <NotificationButton />
     </>
   )
 }
